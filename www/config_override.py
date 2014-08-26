7b53914e3ea3d45c9954e2fba6623fadbd386c50
Ray <ray@aa.com>
2014-08-26 16:54:48 +0800

    測試框架

---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'vovo'
+
+'''
+Override configurations
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+
+}
---
+++ www/wsgiapp.py
@@ -0,0 +1,5 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+__author__ = 'vovo'
+
+

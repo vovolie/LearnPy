0e50064d3de40f00c5709085162aa3c0887f2593
Ray <ray@aa.com>
2014-08-27 18:00:28 +0800

    新增頁面

--- www/urls.py
+++ www/urls.py
@@ -7,8 +7,94 @@ import os, re, time, base64, hashlib, logging
 
 from transwarp.web import get, view, ctx, post, interceptor, seeother, notfound
 from models import User, Blog, Comment
+from config import configs
 
 from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
+
+_COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
+
+
+def make_signed_cookie(id, password, max_age):
+    # build cookie string by: id-expires-md5
+    expires = str(int(time.time() + (max_age or 86400)))
+    L = [id, expires, hashlib.md5('%s-%s-%s-%s' % (id, password, expires, _COOKIE_KEY)).hexdigest()]
+    return '-'.join(L)
+
+def parse_signed_cookie(cookie_str):
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        id, expires, md5 = L
+        if int(expires) < time.time():
+            return None
+        user = User.get(id)
+        if user is None:
+            return None
+        if md5 != hashlib.md5('%s-%s-%s-%s' % (id, user.password, expires, _COOKIE_KEY)).hexdigest():
+            return None
+        return user
+    except:
+        return None
+
+def check_admin():
+    user = ctx.request.user
+    if user and user.admin:
+        return
+    raise APIPermissionError('No permission.')
+
+@interceptor('/')
+def user_interceptor(next):
+    logging.info('try to bind user from session cookie...')
+    user = None
+    cookie = ctx.request.cookies.get(_COOKIE_NAME)
+    if cookie:
+        logging.info('parse session cookie...')
+        user = parse_signed_cookie(cookie)
+        if user:
+            logging.info('bind user <%s> to session...' % user.email)
+    ctx.request.user = user
+    return next()
+
+@interceptor('/manage/')
+def manage_interceptor(next):
+    user = ctx.request.user
+    if user and user.admin:
+        return next()
+    raise seeother('/signin')
+
+@view('signin.html')
+@get('/signin')
+def signin():
+    return dict()
+
+@get('/signout')
+def signout():
+    ctx.response.delete_cookie(_COOKIE_NAME)
+    raise  seeother('/')
+
+
+@api
+@post('/api/authenticate')
+def authenticate():
+    i = ctx.request.input(remember='')
+    email = i.email.strip().lower()
+    password = i.password
+    remember = i.remember
+    user = User.find_first('where email=?', email)
+    if user is None:
+        raise APIError('auth:failed', 'email', 'Invalid email.')
+    elif user.password != password:
+        raise APIError('auth:failed','password','Invalid password.')
+
+    # make session cookie:
+    max_age = 604800 if remember=='true' else None
+    cookie = make_signed_cookie(user.id, user.password, max_age)
+    ctx.response.set_cookie(_COOKIE_NAME, cookie, max_age=max_age)
+    user.password = '******'
+    return user
+
 
 @view('blogs.html')
 @get('/')
